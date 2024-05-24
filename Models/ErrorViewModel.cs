00282d72bc8751b599236b388e6297109343af62
nikkinip <nikkinip2007@gmail.com>
2024-05-24 09:48:16 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAnime.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
